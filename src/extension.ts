0375d56c58a4f5fdd303980db4b060fab76de3af
upizpp <upizpp123@163.com>
2023-07-16 17:36:11 +0800

    first version

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,61 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import path = require("path");
+import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	const config = vscode.workspace.getConfiguration("comline-tool");
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "comline-tool" is now active!');
+	const commandCount = 5;
+	const replaceDict : { [key: string]: () => string } = {
+		workspace() {
+			const workspaceFolders = vscode.workspace.workspaceFolders;
+			if (workspaceFolders && workspaceFolders.length > 0) {
+				return workspaceFolders[0].uri.fsPath;
+			}
+			return "";
+		},
+		file() {
+			const activeEditor = vscode.window.activeTextEditor;
+			if (activeEditor) {
+				return activeEditor.document.uri.fsPath;
+			}
+			return "";
+		},
+		dir() {
+			const activeEditor = vscode.window.activeTextEditor;
+			if (activeEditor) {
+				const fileUri = activeEditor.document.uri;
+				return path.dirname(fileUri.fsPath);
+			}
+			return "";
+		},
+	};
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('comline-tool.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from comline-tool!');
-	});
+	let terminal: vscode.Terminal;
 
-	context.subscriptions.push(disposable);
+	for (let i = 1; i <= commandCount; i++) {
+		let disposable = vscode.commands.registerCommand(
+			`comline-tool.command${i}`,
+			() => {
+				if (
+					terminal === undefined ||
+					terminal.exitStatus !== undefined
+				) {
+					terminal = vscode.window.createTerminal("Terminal");
+					terminal.show();
+				}
+				let command: string = config.get(
+					`command${i}_setting`
+				) as string;
+				for (let key in replaceDict) {
+					if (replaceDict.hasOwnProperty(key)) {
+					  const value = replaceDict[key]();
+					  command = command.replace(`$\{${key}\}`, value);
+					}
+				  }
+				terminal.sendText(command);
+			}
+		);
+
+		context.subscriptions.push(disposable);
+	}
 }
-
-// This method is called when your extension is deactivated
-export function deactivate() {}
